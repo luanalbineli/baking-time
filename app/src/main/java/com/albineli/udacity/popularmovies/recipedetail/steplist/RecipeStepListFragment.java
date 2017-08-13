1dd1f9e09c8e68d9ccc39de62221b0d5dea8df9e
Luan Albineli <crosscountrybrasil@gmail.com>
2017-08-13 16:09:47 -0300

    Implementation of the ingredient list

--- app/src/main/java/com/albineli/udacity/popularmovies/recipedetail/steplist/RecipeStepListFragment.java
+++ app/src/main/java/com/albineli/udacity/popularmovies/recipedetail/steplist/RecipeStepListFragment.java
@@ -2,17 +2,20 @@ package com.albineli.udacity.popularmovies.recipedetail.steplist;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.albineli.udacity.popularmovies.R;
+import com.albineli.udacity.popularmovies.base.BaseFragment;
 import com.albineli.udacity.popularmovies.base.BasePresenter;
 import com.albineli.udacity.popularmovies.base.BaseRecyclerViewFragment;
 import com.albineli.udacity.popularmovies.injector.components.ApplicationComponent;
 import com.albineli.udacity.popularmovies.injector.components.DaggerFragmentComponent;
 import com.albineli.udacity.popularmovies.model.RecipeStepModel;
+import com.viewpagerindicator.TitlePageIndicator;
 
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
@@ -20,10 +23,11 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-public class RecipeStepListFragment extends BaseRecyclerViewFragment<RecipeStepListContract.View> implements RecipeStepListContract.View {
+public class RecipeStepListFragment extends BaseFragment<RecipeStepListContract.View> implements RecipeStepListContract.View {
     public static RecipeStepListFragment getInstance(List<RecipeStepModel> recipeStepList) {
         Bundle bundle = new Bundle();
         bundle.putParcelableArrayList(RECIPE_STEP_LIST_BUNDLE_KEY, new ArrayList<>(recipeStepList));
@@ -48,6 +52,12 @@ public class RecipeStepListFragment extends BaseRecyclerViewFragment<RecipeStepL
     @Inject
     RecipeStepListPresenter mPresenter;
 
+    @BindView(R.id.indicatorRecipeStepList)
+    TitlePageIndicator mIndicator;
+
+    @BindView(R.id.vpRecipeStepList)
+    ViewPager mViewPager;
+
     private List<RecipeStepModel> mRecipeStepList;
 
     @Override
@@ -67,15 +77,20 @@ public class RecipeStepListFragment extends BaseRecyclerViewFragment<RecipeStepL
         mRecipeStepList = getArguments().getParcelableArrayList(RECIPE_STEP_LIST_BUNDLE_KEY);
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_recipe_step_list, container, false);
+
+        ButterKnife.bind(this, rootView);
+
+        return rootView;
+    }
+
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         mPresenter.start(mRecipeStepList);
-    }
-
-    @Override
-    protected void configureRecyclerView(RecyclerView recyclerView) {
-
     }
 }
